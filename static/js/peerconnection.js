fc5ee6f2062ef2bc5b30240a80b7114281ee6ad3
Kazuki Oikawa <k@oikw.org>
2013-06-01 15:08:26 +0900

    PeerConnection経由だとデータ破損が起こる不具合を修正

--- static/js/peerconnection.js
+++ static/js/peerconnection.js
@@ -23,22 +23,35 @@
         if (typeof data == 'string')
             return '0' + data;
         if (data.buffer) data = data.buffer.slice(data.byteOffset, data.byteLength);
+        /*
         if (data.byteLength % 2 == 0)
             return '1' + String.fromCharCode.apply(null, new Uint16Array(data));
         var tmp = new ArrayBuffer(data.byteLength + 1);
         new Uint8Array(tmp).set(new Uint8Array(data));
         return '2' + String.fromCharCode.apply(null, new Uint16Array(tmp));
+        */
+
+        // 上記コメントアウトの方法では化けるのでデータ長は２倍になるが1文字1バイト割り当てで回避
+        return '3' + String.fromCharCode.apply(null, new Uint8Array(data));
     };
     var str2data = function(str) {
         if (str.charAt(0) == '0')
             return str.substring(1);
-        var buf = new ArrayBuffer((str.length - 1) * 2);
-        var bufView = new Uint16Array(buf);
-        for (var i = 1, strLen = str.length; i < strLen; i++)
-            bufView[i - 1] = str.charCodeAt(i);
-        if (str.charAt(0) == '1')
+        if (str.charAt(0) === '3') {
+            var buf = new ArrayBuffer(str.length - 1);
+            var view = new Uint8Array(buf);
+            for (var i = 0; i < str.length - 1; i ++)
+                view[i] = str.charCodeAt(i + 1) & 0xff;
             return buf;
-        return buf.slice(0, buf.byteLength - 1);
+        } else {
+            var buf = new ArrayBuffer((str.length - 1) * 2);
+            var bufView = new Uint16Array(buf);
+            for (var i = 1, strLen = str.length; i < strLen; i++)
+                bufView[i - 1] = str.charCodeAt(i);
+            if (str.charAt(0) == '1')
+                return buf;
+            return buf.slice(0, buf.byteLength - 1);
+        }
     };
 
     var mozPeerDataChannelWrapper = function() {
